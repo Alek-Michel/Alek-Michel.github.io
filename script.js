518b70e66f283cf8a9a8f1e2b0dec6eb3bc7cffd
Alek-Michel <119119113+Alek-Michel@users.noreply.github.com>
2023-03-25 16:13:06 +0100

    29 Mothers day

---
+++ script.js
@@ -0,0 +1,48 @@
+var images = [
+    "sources/1.jpg",
+    "sources/2.jpg",
+    "sources/3.jpg",
+    "sources/4.jpg",
+    "sources/5.jpg",
+    "sources/6.jpg",
+    "sources/7.jpg",
+    "sources/8.jpg",
+    "sources/9.jpg",
+  ];
+  
+  var slideshowImage = document.getElementById("slideshow-image")
+  var currentImageIndex = 0
+  
+  function showSlideshow() {
+    var fullscreen = document.getElementById("fullscreen")
+    var closeBtn = document.getElementById("close-btn")
+  
+    // Show the fullscreen elements
+    fullscreen.style.display = "block"
+    closeBtn.style.display = "block"
+  
+    // Set the first image as the slideshow image
+    slideshowImage.src = images[currentImageIndex]
+    slideshowImage.style.opacity = 1
+  
+    // Advance to the next image every 5 seconds
+    setInterval(function() {
+      // Fade out the current image
+      slideshowImage.style.opacity = 0
+  
+      setTimeout(function() {
+        currentImageIndex = (currentImageIndex + 1) % images.length
+        slideshowImage.src = images[currentImageIndex]
+  
+        // Fade in the next image
+        slideshowImage.style.opacity = 1
+      }, 1000)
+    }, 5000)
+  }
+  
+  function exitFullScreen() {
+    var fullscreen = document.getElementById("fullscreen")
+    var closeBtn = document.getElementById("close-btn")
+    fullscreen.style.display = "none"
+    closeBtn.style.display = "none"
+  }
